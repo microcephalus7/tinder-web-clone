6896abbe30ce789ee28d95b64a859260612030e1
microcephalus7 <microcephalus7@gmail.com>
2021-05-24 17:23:54 +0900

    Add backend/core/utils validiationCheck

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,3 +1,4 @@
+from typing import List
 from django.shortcuts import get_object_or_404
 import jwt
 from django.http import JsonResponse, request
@@ -28,3 +29,10 @@ def tokenCheckDecorator(func):
             return JsonResponse("해당 유저가 프로필을 생성하지 않았습니다", status=404)
         return func(request, *args, **kwargs)
     return wrapper
+
+# 유효성 체크 함수
+def validiationCheck(requiredList:List, requestKeysList:List):
+    requiredListSet = set(requiredList)
+    requestKeysListSet = set(requestKeysList)
+    requestCheck = requiredListSet- requestKeysListSet
+    return requestCheck == set([]) and True or False
