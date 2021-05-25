eeee8d53a22cb3a538484461858d31aa6927779f
microcephalus7 <microcephalus7@gmail.com>
2021-05-25 18:25:17 +0900

    Add backend/community/urls signUp

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, ),
     path('signin', views.signIn),
+    path("signup", views.signUp),
     path('token', views.tokenCheck),
     path("profile", views.profile)
 ]
